f7e44b7878273f17f287d242c5b0aab60824d265
raylee18 <92286971+raylee18@users.noreply.github.com>
2021-10-24 11:46:10 +0700

    Add files via upload

---
+++ program.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+var name = prompt("Masukkan nama anda:");
+var conf = confirm("Hai "+name+"! Apakah kabarmu baik?");
+if(conf == true){
+	alert('Syukurlah!');
+}else{
+	alert('Lho? Semoga baikkan ya!');
+}
